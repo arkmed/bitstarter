dd33f767d551eae2b337f39c3b0488fa151948c2
Mark Taylor <arkmed@gmail.com>
2013-09-28 18:20:44 +0000

    attempt 3

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    fs.readFile('./index.html', function read(err, data)){
+	if(err){
+	    throw err;
+	}
+
+    response.send(data);
 });
 
 var port = process.env.PORT || 5000;
