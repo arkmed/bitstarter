d743d9f14d63af5a0a3bcbdeb5ee5c64ed54fda4
Mark Taylor <arkmed@gmail.com>
2013-09-28 19:10:49 +0000

    attempt 4

--- web.js
+++ web.js
@@ -3,10 +3,15 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    response.send( fs.readFileSync('./index.html', 'encoding'));
+    var content = fs.readFileSync('./index.html','encoding');    
+
+response.send(content);
+
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
